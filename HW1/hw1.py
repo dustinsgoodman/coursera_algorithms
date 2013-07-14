e11783471c727649754edb31f91d4ad8cb9e3aa9
Dustin Goodman <dustin.goodman@clickscape.com>
2013-07-13 23:41:42 -0400

    Update docstring to be width of code

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,10 +1,17 @@
 '''
 Homework 1
 Download the text file here. (Right click and save link as)
-This file contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.
+This file contains all of the 100,000 integers between 1 and
+100,000 (inclusive) in some order, with no integer repeated.
 
-Your task is to compute the number of inversions in the file given, where the ith row of the file indicates the ith entry of an array.
-Because of the large size of this array, you should implement the fast divide-and-conquer algorithm covered in the video lectures. The numeric answer for the given input file should be typed in the space below.
+Your task is to compute the number of inversions in the file
+given, where the ith row of the file indicates the ith entry 
+of an array.
+
+Because of the large size of this array, you should implement
+the fast divide-and-conquer algorithm covered in the video 
+lectures. The numeric answer for the given input file should 
+be typed in the space below.
 
 => 2407905288
 '''
